49213a4dfe7fa499f74ae27d3f45d4180d1c5cad
Tim Britton <ziberath@gmail.com>
2016-03-06 21:32:31 -0600

    Small compile error fix.

--- HordeGame/Physics/Action/StopBodyAction.cs
+++ HordeGame/Physics/Action/StopBodyAction.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using Engine.Physics.Collision;
 using Engine.Physics.Action;
+using Engine.Physics.Movement;
 
 namespace HordeGame.Physics.Action
 {
     public class StopBodyAction : IAction
     {
-        public void performAction(ref CollisionBody c)
+        public void performAction(ref IMoveable c)
         {
             
         }
